60908cf5f9233bbd058e1b84ac8f58ab22c87b9b
Ümit Atalan <umit.atalan@gmail.com>
2022-05-11 14:48:36 +0300

    Generic Repository

--- DataAccessLayer/Abstract/IAboutDal.cs
+++ DataAccessLayer/Abstract/IAboutDal.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Abstract
 {
-    public interface ICategoryDal : IRepository<Category>
+    public interface IAboutDal : IRepository<About>
     {
+
     }
 }
