f6ecef322ef56bd7c8907168ec321e169ccce791
Ümit Atalan <umit.atalan@gmail.com>
2022-05-13 09:43:04 +0300

    FluentValidation

--- BusinessLayer/Concrete/CategoryManager.cs
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -20,28 +20,14 @@ namespace BusinessLayer.Concrete
             _categoryDal = categoryDal;
         }
 
-        //GenericRepository<Category> repo = new GenericRepository<Category>();
-
-        //public List<Category> GetAllBL()
-        //{
-        //    return repo.List();
-        //}
-
-        //public void CategoryAddBL(Category p)
-        //{
-        //    if (p.CategoryName == "" || p.CategoryName.Length <= 3 ||
-        //        p.CategoryDescription == "" || p.CategoryName.Length >= 51)
-        //    {
-        //        //hata mesajı
-        //    }
-        //    else
-        //    {
-        //        repo.Insert(p);
-        //    }
-        //}
         public List<Category> GetCategories()
         {
             return _categoryDal.List();
+        }
+
+        public void CategoryAddBL(Category category)
+        {
+            _categoryDal.Insert(category);
         }
     }
 }
---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori adını boş bırakamazsınız");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklama boş olamaz");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Kategori adı en az 3 karakter olmalıdır.");
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Kategori adı en fazla 20 karakter olmalıdır.");
+        }
+    }
+}
