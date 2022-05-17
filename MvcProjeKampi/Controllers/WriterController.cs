fd6e67482594e4432deec98c32d70d666cb39eda
Ümit Atalan <umit.atalan@gmail.com>
2022-05-17 11:50:09 +0300

    EntityState and Writer Transactions

---
+++ MvcProjeKampi/Controllers/WriterController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager writerManager = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var writers = writerManager.GetWriters();
+            return View(writers);
+        }
+    }
+}
