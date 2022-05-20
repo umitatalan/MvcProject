6353f59fb27c24dd9f59df084be69fb249185f05
Ümit Atalan <umit.atalan@gmail.com>
2022-05-20 11:08:29 +0300

    DropdownList

--- MvcProjeKampi/Controllers/HeadingController.cs
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,30 @@ namespace MvcProjeKampi.Controllers
     public class HeadingController : Controller
     {
         HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
             var headingList = headingManager.GetHeadings();
             return View(headingList);
+        }
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> categories = (from x in categoryManager.GetCategories()
+                                               select new SelectListItem
+                                               {
+                                                   Value = x.CategoryID.ToString(),
+                                                   Text = x.CategoryName
+                                               }).ToList();
+            ViewBag.categories = categories;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddHeading(Heading heading)
+        {
+            heading.HeadingDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            headingManager.HeadingAdd(heading);
+            return RedirectToAction("Index");
         }
     }
 }
