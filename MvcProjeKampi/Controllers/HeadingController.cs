80b53b20c927cc9c6cde77d794d00315b54f1948
Ümit Atalan <umit.atalan@gmail.com>
2022-05-20 10:51:07 +0300

    Heading List

---
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class HeadingController : Controller
+    {
+        HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
+        public ActionResult Index()
+        {
+            var headingList = headingManager.GetHeadings();
+            return View(headingList);
+        }
+    }
+}
