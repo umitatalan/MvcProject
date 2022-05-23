c9f5d9c82e97875abcd815ca708742fd1ba179dc
Ümit Atalan <umit.atalan@gmail.com>
2022-05-23 09:38:22 +0300

    ContentsByHeading

--- MvcProjeKampi/Controllers/ContentController.cs
+++ MvcProjeKampi/Controllers/ContentController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,15 +10,16 @@ namespace MvcProjeKampi.Controllers
 {
     public class ContentController : Controller
     {
-        // GET: Content
+        ContentManager contentManager = new ContentManager(new EfContentDal());
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult ContentByHeading()
+        public ActionResult ContentByHeading(int id)
         {
-            return View();
+            var contents = contentManager.GetListByHeadingId(id);
+            return View(contents);
         }
     }
 }
