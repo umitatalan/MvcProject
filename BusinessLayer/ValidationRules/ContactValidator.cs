6cec9e61619a3498e1e92f2d35073c2c0627bc05
Ümit Atalan <umit.atalan@gmail.com>
2022-05-24 15:35:10 +0300

    Message List

---
+++ BusinessLayer/ValidationRules/ContactValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class ContactValidator : AbstractValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            RuleFor(x => x.UserMail).NotEmpty().WithMessage("Mail adresi boş olamaz.");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konu adını boş bırakamazsınız");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("Kullanıcı adını boş olamaz");
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("Konu adı en az 3 karakter olmalıdır.");
+            RuleFor(x => x.UserName).MinimumLength(3).WithMessage("Kullanıcı adı en az 3 karakter olmalıdır.");
+            RuleFor(x => x.Subject).MaximumLength(50).WithMessage("Konu adı en fazla 50 karakter olmalıdır.");
+        }
+    }
+}
--- MvcProjeKampi/Controllers/ContactController.cs
+++ MvcProjeKampi/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,12 @@ namespace MvcProjeKampi.Controllers
 {
     public class ContactController : Controller
     {
-        // GET: Contact
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        ContactValidator validationRules = new ContactValidator();
         public ActionResult Index()
         {
-            return View();
+            var contacts = contactManager.GetContacts();
+            return View(contacts);
         }
     }
 }
