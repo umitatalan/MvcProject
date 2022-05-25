e941acbc1feddc904627d3f8ab6a03451215236f
Ümit Atalan <umit.atalan@gmail.com>
2022-05-25 11:13:12 +0300

    Message Controller and Inbox of Admin

---
+++ BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class MessageManager : IMessageService
+    {
+        IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public Message GetById(int id)
+        {
+            return _messageDal.Get(x => x.MessageID == id);
+        }
+
+        public List<Message> GetMessages()
+        {
+            return _messageDal.List(x => x.ReceiverMail == "admin@gmail.com");
+        }
+
+        public void MessageAdd(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageDelete(Message message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MessageUpdate(Message message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
